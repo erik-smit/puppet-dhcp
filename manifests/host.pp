e7ea596c1dcc18ebf137ea0d3bb9a387b002691b
erik-smit <smite@zylon.net>
2014-03-18 15:10:27 +0100

    Must pass target to Concat::Fragment

--- manifests/host.pp
+++ manifests/host.pp
@@ -10,6 +10,7 @@ define dhcp::host (
   $dhcp_dir = $dhcp::params::dhcp_dir
 
   concat::fragment { "dhcp.hosts+10_${name}.hosts":
+    target  => "${dhcp_dir}/dhcp.hosts",
     content => template('dhcp/dhcpd.host.erb'),
   }
 
