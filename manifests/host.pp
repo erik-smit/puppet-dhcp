30453df39541fa070ee0b32e88ac7e77dde2541e
erik-smit <smite@zylon.net>
2014-03-18 16:11:45 +0100

    Exec[concat_/etc/dhcp/dhcp.hosts] doesn't seem to be in the catalog

--- manifests/host.pp
+++ manifests/host.pp
@@ -9,8 +9,8 @@ define dhcp::host (
 
   $dhcp_dir = $dhcp::params::dhcp_dir
 
-  concat::fragment { "dhcp.hosts+10_${name}.hosts":
-    target  => "${dhcp_dir}/dhcp.hosts",
+  concat::fragment { "dhcpd.hosts+10_${name}.hosts":
+    target  => "${dhcp_dir}/dhcpd.hosts",
     content => template('dhcp/dhcpd.host.erb'),
   }
 
